5518b76f1e5a9f76e6e29cd3613f8a48abd2fd7c
caiofabiogomes <caiofabio250@gmail.com>
2025-06-01 17:48:04 -0300

    Ajustando código para passar nos testes

--- OrderService.Domain/ValueObjects/OrderStatus.cs
+++ OrderService.Domain/ValueObjects/OrderStatus.cs
@@ -1,4 +1,6 @@
 ﻿
+using OrderService.Domain.Exceptions;
+
 namespace OrderService.Domain.ValueObjects
 {
     public sealed class OrderStatus : IEquatable<OrderStatus>
@@ -6,6 +8,7 @@ namespace OrderService.Domain.ValueObjects
         public OrderStatus()
         {
             Status = Enums.OrderStatus.Pending;
+            Justification = string.Empty;
         }
 
         private OrderStatus(Enums.OrderStatus status, string justification)
@@ -19,11 +22,83 @@ namespace OrderService.Domain.ValueObjects
 
         public string Justification { get; private set; }
 
-        public OrderStatus Accepted() => new OrderStatus(Enums.OrderStatus.Accepted, "");
-        public OrderStatus Rejected() => new OrderStatus(Enums.OrderStatus.Rejected, "");
-        public OrderStatus Processing() => new OrderStatus(Enums.OrderStatus.Processing, "");
-        public OrderStatus Completed() => new OrderStatus(Enums.OrderStatus.Completed, "");
-        public OrderStatus Cancelled(string justification) => new OrderStatus(Enums.OrderStatus.Cancelled, justification);
+        public OrderStatus Accepted()
+        {
+            var invalidsStatus = new List<Enums.OrderStatus>
+            {
+                Enums.OrderStatus.Rejected,
+                Enums.OrderStatus.Cancelled,
+                Enums.OrderStatus.Processing
+            };
+
+            if (invalidsStatus.Contains(Status))
+                throw new DomainException(string.Format(DomainExceptionMessages.InvalidOrderStatusTransitionToAccepted, Status.ToString()));
+
+            return new OrderStatus(Enums.OrderStatus.Accepted, "");
+        }
+
+        public OrderStatus Rejected()
+        {
+            var invalidsStatus = new List<Enums.OrderStatus>
+            {
+                Enums.OrderStatus.Accepted,
+                Enums.OrderStatus.Cancelled,
+                Enums.OrderStatus.Processing
+            };
+
+            if (invalidsStatus.Contains(Status))
+                throw new DomainException(string.Format(DomainExceptionMessages.InvalidOrderStatusTransitionToRejected, Status.ToString()));
+
+            return new OrderStatus(Enums.OrderStatus.Rejected, "");
+        }
+
+        public OrderStatus Processing() 
+        {
+            var invalidsStatus = new List<Enums.OrderStatus>
+            {
+                Enums.OrderStatus.Pending,
+                Enums.OrderStatus.Rejected,
+                Enums.OrderStatus.Cancelled,
+                Enums.OrderStatus.Completed
+            };
+
+            if (invalidsStatus.Contains(Status))
+                throw new DomainException(string.Format(DomainExceptionMessages.InvalidOrderStatusTransitionToProcessing, Status.ToString()));
+
+            return new OrderStatus(Enums.OrderStatus.Processing, ""); 
+        }
+
+        public OrderStatus Completed() 
+        {
+            var invalidsStatus = new List<Enums.OrderStatus>
+            {
+                Enums.OrderStatus.Pending,
+                Enums.OrderStatus.Rejected,
+                Enums.OrderStatus.Cancelled,
+                Enums.OrderStatus.Accepted
+            };
+            if (invalidsStatus.Contains(Status))
+                throw new DomainException(string.Format(DomainExceptionMessages.InvalidOrderStatusTransitionToCompleted, Status.ToString()));
+
+            return new OrderStatus(Enums.OrderStatus.Completed, "");
+        }
+
+        public OrderStatus Cancelled(string justification) 
+        {
+            var invalidsStatus = new List<Enums.OrderStatus>
+            {
+                Enums.OrderStatus.Pending,
+                Enums.OrderStatus.Rejected,
+                Enums.OrderStatus.Processing
+            };
+            if (invalidsStatus.Contains(Status))
+                throw new DomainException(string.Format(DomainExceptionMessages.InvalidOrderStatusTransitionToCancel, Status.ToString()));
+
+            if (string.IsNullOrWhiteSpace(justification))
+                throw new DomainException(DomainExceptionMessages.InvalidOrderStatusTransitionToCancelledWithoutJustification);
+
+            return new OrderStatus(Enums.OrderStatus.Cancelled, justification); 
+        }
 
         public override bool Equals(object? obj) => Equals(obj as OrderStatus);
 
