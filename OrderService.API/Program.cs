12c68ef2c61b6df1e369cf60d424db67bb38bfa4
caiofabiogomes <caiofabio250@gmail.com>
2026-05-23 16:24:26 -0300

    adding trace

--- OrderService.API/Program.cs
+++ OrderService.API/Program.cs
@@ -1,12 +1,46 @@
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using OrderService.Application;
 using OrderService.Infraestructure;
 using OrderService.Infraestructure.Persistence;
+using Serilog;
+using Serilog.Enrichers.Span;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var serviceName = "FastTech.OrdersAPI";
 
 var configuration = builder.Configuration;
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(context.Configuration) // Mantém a leitura do appsettings
+        .Enrich.FromLogContext()
+        .Enrich.WithSpan() // 👇 1. Extrai o TraceId do OpenTelemetry/Activity
+        .WriteTo.Console(outputTemplate: 
+            "[{Timestamp:HH:mm:ss} {Level:u3}] [TraceId: {TraceId}] {Message:lj}{NewLine}{Exception}"); // 👇 2. Obriga a imprimir no terminal
+});
+
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation()
+            .AddSource("MassTransit")
+            .AddOtlpExporter(options =>
+            {
+                // 👇 FORÇANDO A URL E O PROTOCOLO 👇
+                options.Endpoint = new Uri("http://otel-collector:4317");
+                options.Protocol = OtlpExportProtocol.Grpc;
+            });
+    });
 
 builder.Services.AddControllers();
 builder.Services
